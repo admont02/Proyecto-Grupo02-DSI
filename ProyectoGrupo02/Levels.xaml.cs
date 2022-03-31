11ef04078961097419498f6f16fd64ff61b9de7a
ssimoanto <simoanto@ucm.es>
2022-03-31 12:17:41 +0200

    añadida pagina de niveles :lemon:

--- ProyectoGrupo02/Levels.xaml.cs
+++ ProyectoGrupo02/Levels.xaml.cs
@@ -13,28 +13,27 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace ProyectoGrupo02
 {
     /// <summary>
-    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// Una página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Levels : Page
     {
-        public MainPage()
+        public Levels()
         {
             this.InitializeComponent();
         }
-
-        private void Play_Click(object sender, RoutedEventArgs e)
+        private void Click_Back(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Levels));
+            Frame.Navigate(typeof(MainPage));
         }
-       
-        private void Settings_OnClick(object sender, RoutedEventArgs e)
+        private void Click_Level(object sender, RoutedEventArgs e)
         {
-            Frame.Navigate(typeof(Settings));
+            Frame.Navigate(typeof(InGame));
         }
+        // Click_Back
     }
 }
